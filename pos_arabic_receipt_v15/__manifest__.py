b7947391281b3cb05f14a949ca28f9b7aee96d61
ajmal <ajmalc6705@gmail.com>
2022-07-18 11:52:40 +0530

    js added in manifest

--- pos_arabic_receipt_v15/__manifest__.py
+++ pos_arabic_receipt_v15/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'POS Arabic Receipt',
     'summary': 'POS arabic receipt as per the industrial Standard',
     'description': """POS arabic receipt as per the industrial Standard""",
-    "version": "14.1.1.1",
+    "version": "15.1.1.1",
     'sequence': 1,
     'email': 'ajmalc6705@gmail.com',
     'website': '',
@@ -10,15 +10,27 @@
     'author': 'Ajmal',
     'price': 35,
     'currency': 'EUR',
-    'license': 'OPL-1',
+    'license': 'LGPL-3',
     "live_test_url": "",
     'depends': ['point_of_sale'],
     'qweb': ['static/src/xml/templates.xml'],
-    'data': ['views/pos_receipt_template.xml',
-             'views/product_view.xml'
-             ],
+    'data': [
+        # 'views/pos_receipt_template.xml',
+        'views/product_view.xml'
+    ],
     'images': ['static/description/main_screenshot.png'],
     'demo': [],
+    #
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_arabic_receipt_v15/static/src/js/models.js',
+        ],
+        'web.assets_qweb': [
+            'pos_arabic_receipt_v15/static/src/xml/templates.xml',
+        ],
+
+    },
+
     'installable': True,
     'auto_install': False,
     'application': True,
