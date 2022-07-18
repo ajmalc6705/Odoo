a27c1e809320fb1c8ee46072ed8a30ab91a2811d
ajmal <ajmalc6705@gmail.com>
2022-07-18 11:11:41 +0530

    Pos receipt

---
+++ pos_arabic_receipt_v15/static/src/js/models.js
@@ -0,0 +1,12 @@
+odoo.define("pos_arabic_receipt_v14.models", function (require) {
+"use strict";
+
+var models = require('point_of_sale.models');
+//console.log("=====models====================>>>>>>>>>>>>>>>.");
+
+models.load_fields('res.company', ['street','street2','city','state_id','vat']);
+// models.load_fields('res.company', ['street','street2','city','state_id','vat', 'name_arabic', 'street_arabic', 'street2_arabic', 'city_arabic', 'state_arabic']);
+// models.load_fields('product.product',['name']);
+models.load_fields('product.product',['name','name_arabic']);
+
+})
